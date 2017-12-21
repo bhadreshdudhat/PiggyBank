9d3f975c8e33cf95feca05261560a8e0db4018d3
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2017-12-21 17:14:25 -0600

    Final Closures
    3 different closure working independently

--- Closures_v1.js
+++ Closures_v1.js
@@ -2,7 +2,8 @@
 //  inner functions can access outer function's variable in shared fashion
 
 function f1(){
-   var i=0;// only accesible to inner functions (f2 and f3)
+    var i=0;// only accesible to inner functions (f2 and f3)
+    //var i=0   and not just   i=0;
     //-----f1.i=0;//accesible to whole world
 
     //making function as variable so that it can be accessed from outside the function
@@ -17,15 +18,23 @@ function f1(){
     };
 }
 
-
-
 f1();console.log("------Closure 1 ------");
 var g1=f1.f2;
 var g2=f1.f3;
-g1();g1();g2();
 
 f1();console.log("------Closure 2 ------");
-g1();g1();g2();
+var h1=f1.f2;
+var h2=f1.f3;
 
 f1();console.log("------Closure 3 ------");
-g1();g1();g2();
+var j1=f1.f2;
+var j2=f1.f3;
+
+
+g1();g1();g1();console.log("------");
+h1();h1();h1();console.log("------");
+j1();j1();j1();console.log("------");
+
+g1();g1();g2();console.log("------");
+h1();h1();h2();console.log("------");
+j1();j1();j2();console.log("------");
