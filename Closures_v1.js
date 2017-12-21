a24231dc260d3e76dfd7f79ff5fa34398cac9cd6
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2017-12-21 14:27:23 -0600

    Access inner function from outside world

--- Closures_v1.js
+++ Closures_v1.js
@@ -2,20 +2,23 @@
 //  inner functions can access outer function's variable in shared fashion
 
 function f1(){
-    var i=0;
+    i=0;// only accesible to inner functions (f2 and f3)
+    //-----f1.i=0;//accesible to whole world
 
-    function f2(){
+    //making function as variable so that it can be accessed from outside the function
+    f1.f2=function(){
         i++;
         console.log(i);
-    }
+    };
 
-    function f3(){
+    f1.f3=function(){
         i--;
         console.log(i);
-    }
+    };
 
-    f2();f2();f2();
-    f3(); f3(); f2();
 }
 
 f1();
+f1.f2()
+f1.f3();
+f1.f2();
